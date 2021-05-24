17b79491b47a7debf5382a48bc5db1565ccca5f6
elon21 <elongated21@gmail.com>
2021-05-24 21:38:54 +0300

    completed useContext

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,11 +1,14 @@
+import { useContext } from 'react'
 import StudentItem from './StudentItem'
+import { StudentContext } from '../context/StudentContext'
 
-const StudentList = ({ students, changeColor }) => {
+const StudentList = () => {
+  const { students } = useContext(StudentContext);
   return (
     <>
       <h2>Student List</h2>
       {students.map((student) => (
-        <StudentItem key={student.name} student={student} changeColor={changeColor} />
+        <StudentItem key={student.name} student={student} />
       ))}
     </>
   )
